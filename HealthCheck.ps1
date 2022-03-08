b96a52160280672ab76d3febb90aadb2a9a48b04
sanjeewa.senevirathna <sanjeewa.senevirathna@mcmedisoft.com>
2022-03-08 18:18:01 +0530

    add forech json file

--- HealthCheck.ps1
+++ HealthCheck.ps1
@@ -1,16 +1,19 @@
 ﻿Write-Host "Health Check Validation"
 
 
-$MachineName1 = "VM-1" #$args[1]
-$ApplicationName1 = "keycloak" #keycloak Env
-$ApplicationURL1 = "https://reqbin.com/echo/get/json" #
-$ReqType1 = "GET"
 
-$MachineName2 = "VM-2"
-$ApplicationName2 = "dashboard" #Reporting Env 
-$ApplicationURL2 = "https://reqbin.com/echo/post/json" #
-$ReqType2 = "POST"
-$Body2 = @{ 
+
+
+$machineName1 = "VM-1" #$args[1]
+$applicationName1 = "keycloak" #keycloak Env
+$applicationURL1 = "https://reqbin.com/echo/get/json" #
+
+$endpointType 
+
+$machineName2 = "VM-2"
+$applicationName2 = "dashboard" #Reporting Env 
+$applicationURL2 = "https://reqbin.com/echo/post/json" #
+$body2 = @{ 
   Id= 78912
   Customer= "Jason Sweet"
   Quantity= 1
@@ -22,13 +25,12 @@ function getcall([string]$url) {
     {
         $responseData = Invoke-WebRequest -Uri $url 
         Write-Host "responseData" $responseData
-        #$result = ConvertFrom-Json -InputObject $responseData.Content
-
-        return $responseData
+        $statusCode = [int]$responseData.StatusCode
+        return $statusCode
     }
     catch 
     {
-        throw $_
+        return [int]$_.Exception.Response.StatusCode
     }
    
 }
@@ -36,68 +38,78 @@ function getcall([string]$url) {
 function callpost([string]$url,[string]$body) {
     try 
     {
-        Write-Host $url
+        Write-Host "-----"
+        Write-Host "2nd endpoint-"  $url
       
-
-        # Step 4. Make the POST request
         $responseData = Invoke-RestMethod -Method 'Post' -Uri $url -Body $body
-        ###$result = ConvertFrom-Json $responseData.Content
-        return $responseData
+        $statusCode = [int]$responseData.StatusCode
+        $result = $responseData | ConvertFrom-Json
+        Write-Host "text" + $result
+        return $statusCode
 
     }
     catch 
     {
-        throw $_
+        return [int]$_.Exception.Response.StatusCode
     }
    
 }
 
-function Send-Email([string]$Body) {
+function Send-Email([string]$bodyContent1,[string]$bodyContent2) {
     $From = "sanjeewa.senevirathna@mcmedisoft.com"
-    $To = "sancyclops@gmail.com”
+    $To = "sanjeewa.senevirathna@mcmedisoft.com”
     $Cc = "stephan.desilva@mcmedisoft.com"
     $Attachment = "C:\Users\SanjeewaSenevirathna\Pictures\800px_COLOURBOX11070263.jpg"
-    $Subject = "Photos of Drogon"
-    $Body = "<h2>Guys, look at these pics of Drogon!</h2><br><br>"
-    $Body += “He is so cute!”
+    $Subject = "Health Check"
+    $Body = "<h1>Server Status!</h1><br><br>"
+    $Body += "<p><strong>First Call:</strong> $bodyContent1 </p>”
+    $Body += " <p><strong>Second Call:</strong> $bodyContent2 </p>”
     $SMTPServer = "smtp.office365.com"
     $SMTPPort = "587"
     $mypasswd = ConvertTo-SecureString "Dn5vlPP6" -AsPlainText -Force
     $mycreds = New-Object System.Management.Automation.PSCredential ("sanjeewa.senevirathna@mcmedisoft.com", $mypasswd)
 
-    Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $mycreds -Attachments $Attachment
+    Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject 
+                    -Body $Body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort 
+                    -UseSsl -Credential $mycreds -Attachments $Attachment
+}
+ 
+$json = Get-Content $scriptPath"\request.json" | Out-String | ConvertFrom-Json
+
+ForEach ($User in $json){
+    #"User: {0}" -f $User.vms
+
+    ForEach ($Role in $User.vms){
+        Write-Host  $Role.machineName
+    }
 }
 
 
 
 
-
-
- 
-
-Write-Host $ApplicationURL1
-
 $data1 = getcall -url $ApplicationURL1
 
 Write-Host ConvertFrom-JsonNewtonsoft $data1
 
-$dataToDict1 =  -join ($MachineName1," - ",$ApplicationName1," - ",$data1);
-Write-Host $dataToDict1
+$responseResult1 =  -join ("1st Endpoint - " , $MachineName1," - ",$ApplicationName1," - ",$data1);
+Write-Host $responseResult1
 Start-Sleep -s 2
+
+#if validate status = false {
 
 $data2 = callpost -url $ApplicationURL2 -body $Body2
-Write-Host $MachineName2 "-" $ApplicationName2 "-" $data2
+$responseResult2 =  -join ("2st Endpoint - " , $MachineName2," - ",$ApplicationName2," - ",$data2);
+Write-Host $responseResult2
 Start-Sleep -s 2
 
-$emailText = "test" ##-join ($data1 , $data2)
+#}
 
-Send-Email $emailText
-#Invoke-WebRequest -Uri $ApplicationURL1 -Method Get
-#Write-Host $dataToDict |  Format-Table -Property Title, pubDate
+responseResult = 
 
-#Send-MailMessage -From 'User01 <sanjeewa.senevirathna@mcmedisoft.com>' -To 'User02 <sancyclops@gmail.com>' -Subject $ApplicationName1 + "is not accessbile on " + $MachineName1
-#Send-Email -Body "test"
+Send-Email -bodyContent1 $responseResult1 -bodyContent2 $responseResult2
+ 
 
+ 
 
 
 
---
+++ test.ps1
@@ -0,0 +1,26 @@
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+Write-Host "path -"$scriptPath
+
+
+$json = Get-Content $scriptPath"\request.json" | Out-String | ConvertFrom-Json
+
+ForEach ($User in $json){
+    #"User: {0}" -f $User.vms
+
+    ForEach ($Role in $User.vms){
+        Write-Host  $Role.machineName
+    }
+}
+
+
+#foreach ($i in $json.PSObject.Properties){
+#      write-host $i.name #"-" $i.value 
+#}
+
+
+
+#foreach ($obj in $json.PSObject.Properties) {
+#    $obj.Name
+#    $obj.Value
+#}
